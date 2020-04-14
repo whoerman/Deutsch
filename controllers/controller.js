b2044bd35f7f74834a77ae3044925af249e766a9
Walter Hoerman <whoerman@gmail.com>
2020-04-14 15:18:39 -0400

    more pages and routes

--- controllers/controller.js
+++ controllers/controller.js
@@ -18,6 +18,25 @@ router.get("/nouns", function(req, res) {
 
 router.get("/phrases", function(req, res) {
     res.render("phrases")
-})
+});
+
+router.get("/verbs", function(req, res) {
+    res.render("verbs")
+});
+
+router.get("/adverbs", function(req, res) {
+    res.render("adverbs")
+});
+
+router.get("/adjectives", function(req, res) {
+    res.render("adjectives")
+});
+
+router.get("/otherparts", function(req, res) {
+    res.render("otherparts")
+});
+
+
+
 
 module.exports = router
