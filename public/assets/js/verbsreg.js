221e27f96aa1d5d5ccd2655e12c64c19fc1fc398
Walter Hoerman <whoerman@gmail.com>
2020-04-16 10:04:34 -0400

    regular verb processing

---
+++ public/assets/js/verbsreg.js
@@ -0,0 +1,60 @@
+console.log("run verbreg.js");
+
+//Click button
+$("#newVerb").on("click", function (event) {
+  event.preventDefault();
+
+ //make sure all the forms have data
+ if (
+  !$("#verb").val() ||
+  !$("#englishdefinition").val() 
+) {
+  alert("Looks like your forgot to input something!");
+  return;
+};
+
+//seperate form groups becuase of multiple handlebars
+let newVerbText = {
+  germanverb: $("#verb").val().trim(),
+  englishphrase: $("#englishdefinition").val().trim(),
+};
+
+let newVerbSituation = {
+  athome: $("#athomeselect").val().trim(),
+  atwork: $("#atworkselect").val().trim(),
+  dining: $("#diningselect").val().trim(),
+  hotel: $("#hotelselect").val().trim(),
+  travel: $("#travelselect").val().trim(),
+  people: $("#peopleselect").val().trim(),
+  events: $("#eventsselect").val().trim(),
+  shopping: $("#shoppingselect").val().trim(),
+};
+
+//put all the data into one noun object
+let newVerbData = {
+  ...newVerbText,
+  ...newVerbSituation
+};
+
+//change yes no responses to true false (object to array first)
+const newVerbArray = Object.entries(newVerbData);
+newVerbArray.forEach(function (index) {
+  if (index[1] === "Yes") {
+    index[1] = true
+  };
+  if (index[1] === "No") {
+    index[1] = false
+  }
+});
+
+//changing the array back to an object
+let finalVerbObj = newVerbArray.reduce(function (aspect, curr) {
+  aspect[curr[0]] = curr[1];
+  return aspect;
+}, {});
+
+console.log(finalVerbObj);
+
+console.log ("then i will post finalVerbObj")
+});
+
