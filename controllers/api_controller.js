4b5391e88b4b1e5705bb63b4a7b831d31136a61a
Walter Hoerman <whoerman@gmail.com>
2020-04-19 21:44:48 -0400

    fixing verb schema

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -2,50 +2,62 @@ var express = require('express')
 var router = express.Router();
 const db = require("../models");
 
-router.post("/noun", function(req, res) {
-    const noun = req.body;
-    db.Noun.create(req.body).then(function(dbNoun) {
-        res.json(dbNoun);
-      }).catch ( function(err) {
-        res.sendStatus(err.statuscode).send(err.message).end()
-      });
+//POST Routes
+router.post("/noun", function (req, res) {
+  const noun = req.body;
+  db.Noun.create(req.body).then(function (dbNoun) {
+    res.json(dbNoun);
+  }).catch(function (err) {
+    res.sendStatus(err.statuscode).send(err.message).end()
+  });
 });
 
-router.post("/phrase", function(req, res) {
-    const phrase = req.body;
-    db.Phrase.create(req.body).then(function(dbPhrase) {
-        res.json(dbPhrase);
-      }).catch ( function(err) {
-        res.sendStatus(err.statuscode).send(err.message).end()
-      });
+router.post("/phrase", function (req, res) {
+  const phrase = req.body;
+  db.Phrase.create(req.body).then(function (dbPhrase) {
+    res.json(dbPhrase);
+  }).catch(function (err) {
+    res.sendStatus(err.statuscode).send(err.message).end()
+  });
 });
 
-router.post("/adjective", function(req, res) {
+router.post("/adjective", function (req, res) {
   const adjective = req.body;
-  db.Adjective.create(req.body).then(function(dbAdjective) {
-      res.json(dbAdjective);
-    }).catch ( function(err) {
-      res.sendStatus(err.statuscode).send(err.message).end()
-    });
+  db.Adjective.create(req.body).then(function (dbAdjective) {
+    res.json(dbAdjective);
+  }).catch(function (err) {
+    res.sendStatus(err.statuscode).send(err.message).end()
+  });
 });
 
-router.post("/adverb", function(req, res) {
+router.post("/adverb", function (req, res) {
   const adverb = req.body;
-  db.Adverb.create(req.body).then(function(dbAdverb) {
-      res.json(dbAdverb);
-    }).catch ( function(err) {
-      res.sendStatus(err.statuscode).send(err.message).end()
-    });
+  db.Adverb.create(req.body).then(function (dbAdverb) {
+    res.json(dbAdverb);
+  }).catch(function (err) {
+    res.sendStatus(err.statuscode).send(err.message).end()
+  });
 });
 
-router.post("/verbreg", function(req, res) {
+router.post("/verbreg", function (req, res) {
   const verbreg = req.body;
-  db.Verbreg.create(req.body).then(function(dbVerbreg) {
-      res.json(dbVerbreg);
-    }).catch ( function(err) {
-      res.sendStatus(err.statuscode).send(err.message).end()
-    });
+  db.Verbreg.create(req.body).then(function (dbVerbreg) {
+    res.json(dbVerbreg);
+  }).catch(function (err) {
+    res.sendStatus(err.statuscode).send(err.message).end()
+  });
 });
+
+//GET Routes
+// router.get("/api/noun", function (req, res) {
+//   db.Noun.findAll({
+//     where: {
+//       singular: true,
+//     }
+//   }).then(function (nounlist) {
+//     console.log(nounlist)
+//   });
+// });
 
 
 module.exports = router;
--- models/verbreg.js
+++ models/verbreg.js
@@ -12,6 +12,5 @@ module.exports = function(sequelize, DataTypes) {
         shopping: DataTypes.BOOLEAN
     });
 
-  
     return Verbreg;
   };
